d466e2b1ba34e4391cf9a287f95ce537460e403e
peijane27 <107615922+peijane27@users.noreply.github.com>
2022-09-16 11:30:24 +0800

    new

---
+++ te.py
@@ -0,0 +1,25 @@
+print(123)
+
+print(123*12212*123.2)
+
+
+test=6434<777777
+print(30//10)
+PI=3.14t(5)a')
+
+def tea(love,drink='珍奶'):
+    print('愛喝:',love,'常喝:',drink,sep="")
+    
+
+print(type(tea))
+
+
+
+
+
+
+
+
+
+
+
---
+++ tooo.py
@@ -0,0 +1,24 @@
+print(123)
+
+print(
+
+test=6434<777777
+print(30//10)
+PI=3.14t(5)a')
+
+def tea(love,drink='珍奶'):
+    print('愛喝:',love,'常喝:',drink,sep="")
+    
+
+print(type(tea))
+
+
+
+
+
+
+
+
+
+
+
